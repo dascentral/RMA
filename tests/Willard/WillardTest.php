24728862dc98f01b785c49faa1c1d22a57a4f6f2
Douglas Smith <smithda@gmail.com>
2018-03-08 15:02:59 -0500

    Adding the first unit tests

---
+++ tests/Willard/WillardTest.php
@@ -0,0 +1,27 @@
+<?php
+
+use Dascentral\Rma\Willard;
+use PHPUnit\Framework\TestCase;
+
+class ZttpTest extends TestCase
+{
+    /** @test */
+    public function it_calculates_the_first_sunday_of_the_year()
+    {
+        $sunday = Willard::firstSundayOfYear(2018);
+        $this->assertEquals('2018-01-07', $sunday->format('Y-m-d'));
+
+        $sunday = Willard::firstSundayOfYear(2017);
+        $this->assertEquals('2017-01-01', $sunday->format('Y-m-d'));
+    }
+
+    /** @test */
+    public function it_calculates_the_first_week_of_the_year()
+    {
+        $sunday = Willard::firstWeekOfYear(2018);
+        $this->assertEquals('2017-12-31', $sunday->format('Y-m-d'));
+
+        $sunday = Willard::firstWeekOfYear(2017);
+        $this->assertEquals('2017-01-01', $sunday->format('Y-m-d'));
+    }
+}
