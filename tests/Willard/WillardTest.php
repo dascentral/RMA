18359096c381b49f80a3c865c2d147285a440742
Douglas Smith <smithda@gmail.com>
2019-03-12 14:41:43 -0400

    Adding tests for human()

--- tests/Willard/WillardTest.php
+++ tests/Willard/WillardTest.php
@@ -30,4 +30,23 @@ class WillardTest extends TestCase
         $sunday = Willard::firstWeekOfYear(2017);
         $this->assertEquals('2017-01-01', $sunday->format('Y-m-d'));
     }
+
+    /** @test */
+    public function it_calculates_my_version_of_diff_for_humans()
+    {
+        $date = Date('Y-m-d');
+        $this->assertEquals('Today', Willard::human($date));
+
+        $date = Carbon::now()->addDays(1)->format('Y-m-d');
+        $this->assertEquals('Tomorrow', Willard::human($date));
+
+        $date = Carbon::now()->subDays(1)->format('Y-m-d');
+        $this->assertEquals('Yesterday', Willard::human($date));
+
+        // Day name
+
+        // Last day name
+
+        // ELSE
+    }
 }
