28a66d1de09cdeffedc8654e5b04dd303ee1f5b2
Douglas Smith <smithda@gmail.com>
2019-12-17 16:51:23 -0500

    Adding tests for the midnight() method

---
+++ tests/Willard/WillardMignightTest.php
@@ -0,0 +1,31 @@
+<?php
+
+use Carbon\Carbon;
+use Dascentral\Rma\Willard;
+use PHPUnit\Framework\TestCase;
+
+class WillardMidnightTest extends TestCase
+{
+    /** @test */
+    public function it_calculates_midnight_when_no_date_is_provided()
+    {
+        $midnight = Willard::midnight();
+        $compare = Date('Y-m-d') . ' 00:00:00';
+        $this->assertEquals($compare, $midnight->toDateTimeString());
+    }
+
+    /** @test */
+    public function it_calculates_midnight_when_a_date_is_provided()
+    {
+        $midnight = Willard::midnight('2030-09-12');
+        $this->assertEquals('2030-09-12 00:00:00', $midnight->toDateTimeString());
+    }
+
+    /** @test */
+    public function it_calculates_midnight_when_a_carbon_object_is_provided()
+    {
+        $carbon = Carbon::parse('2030-09-12');
+        $midnight = Willard::midnight($carbon);
+        $this->assertEquals('2030-09-12 00:00:00', $midnight->toDateTimeString());
+    }
+}
