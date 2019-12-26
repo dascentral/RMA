590074c8da5b8e71a19a11f7c5601c2f634fdd20
Douglas Smith <smithda@gmail.com>
2019-12-26 08:50:25 -0500

    Setting time on each weekday to midnight

--- tests/Willard/WillardWeekdaysTest.php
+++ tests/Willard/WillardWeekdaysTest.php
@@ -12,4 +12,14 @@ class WillardWeekdaysTest extends TestCase
         $compare = Willard::sunday()->addDay()->format('l');
         $this->assertEquals($compare, $weekdays[0]->format('l'));
     }
+
+    /** @test */
+    public function it_returns_midnight_for_each_weekday()
+    {
+        $weekdays = Willard::weekdays(5);
+        foreach ($weekdays as $weekday) {
+            $compare = '00:00:00';
+            $this->assertEquals($compare, $weekday->format('H:i:s'));
+        }
+    }
 }
