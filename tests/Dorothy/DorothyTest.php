57580a093ac5442322d528a13b6043538f648c56
Douglas Smith <douglas.smith@akqa.com>
2020-07-25 12:51:18 -0400

    Testing roundsToNearest

--- tests/Dorothy/DorothyTest.php
+++ tests/Dorothy/DorothyTest.php
@@ -8,36 +8,57 @@ class DorothyTest extends TestCase
     /** @test */
     public function it_rounds_down_correctly()
     {
-        $this->assertEquals(50, Dorothy::roundDowntoNearest(50, 5));
-        $this->assertEquals(50, Dorothy::roundDowntoNearest(51, 5));
-        $this->assertEquals(50, Dorothy::roundDowntoNearest(52, 5));
-        $this->assertEquals(50, Dorothy::roundDowntoNearest(53, 5));
-        $this->assertEquals(50, Dorothy::roundDowntoNearest(54, 5));
-        $this->assertEquals(55, Dorothy::roundDowntoNearest(55, 5));
-        $this->assertEquals(55, Dorothy::roundDowntoNearest(56, 5));
-        $this->assertEquals(55, Dorothy::roundDowntoNearest(57, 5));
-        $this->assertEquals(55, Dorothy::roundDowntoNearest(58, 5));
-        $this->assertEquals(55, Dorothy::roundDowntoNearest(59, 5));
-        $this->assertEquals(60, Dorothy::roundDowntoNearest(60, 5));
+        $this->assertEquals(50, Dorothy::roundDownToNearest(50, 5));
+        $this->assertEquals(50, Dorothy::roundDownToNearest(51, 5));
+        $this->assertEquals(50, Dorothy::roundDownToNearest(52, 5));
+        $this->assertEquals(50, Dorothy::roundDownToNearest(53, 5));
+        $this->assertEquals(50, Dorothy::roundDownToNearest(54, 5));
+        $this->assertEquals(55, Dorothy::roundDownToNearest(55, 5));
+        $this->assertEquals(55, Dorothy::roundDownToNearest(56, 5));
+        $this->assertEquals(55, Dorothy::roundDownToNearest(57, 5));
+        $this->assertEquals(55, Dorothy::roundDownToNearest(58, 5));
+        $this->assertEquals(55, Dorothy::roundDownToNearest(59, 5));
+        $this->assertEquals(60, Dorothy::roundDownToNearest(60, 5));
+    }
+
+    /** @test */
+    public function it_rounds_correctly()
+    {
+        $this->assertEquals(45, Dorothy::roundToNearest(45, 5));
+        $this->assertEquals(45, Dorothy::roundToNearest(46, 5));
+        $this->assertEquals(45, Dorothy::roundToNearest(47, 5));
+        $this->assertEquals(50, Dorothy::roundToNearest(48, 5));
+        $this->assertEquals(50, Dorothy::roundToNearest(49, 5));
+        $this->assertEquals(50, Dorothy::roundToNearest(50, 5));
+        $this->assertEquals(50, Dorothy::roundToNearest(51, 5));
+        $this->assertEquals(50, Dorothy::roundToNearest(52, 5));
+        $this->assertEquals(55, Dorothy::roundToNearest(53, 5));
+        $this->assertEquals(55, Dorothy::roundToNearest(54, 5));
+        $this->assertEquals(55, Dorothy::roundToNearest(55, 5));
+        $this->assertEquals(55, Dorothy::roundToNearest(56, 5));
+        $this->assertEquals(55, Dorothy::roundToNearest(57, 5));
+        $this->assertEquals(60, Dorothy::roundToNearest(58, 5));
+        $this->assertEquals(60, Dorothy::roundToNearest(59, 5));
+        $this->assertEquals(60, Dorothy::roundToNearest(60, 5));
     }
 
     /** @test */
     public function it_rounds_up_correctly()
     {
-        $this->assertEquals(50, Dorothy::roundUptoNearest(46, 5));
-        $this->assertEquals(50, Dorothy::roundUptoNearest(47, 5));
-        $this->assertEquals(50, Dorothy::roundUptoNearest(48, 5));
-        $this->assertEquals(50, Dorothy::roundUptoNearest(49, 5));
-        $this->assertEquals(50, Dorothy::roundUptoNearest(50, 5));
-        $this->assertEquals(55, Dorothy::roundUptoNearest(51, 5));
-        $this->assertEquals(55, Dorothy::roundUptoNearest(52, 5));
-        $this->assertEquals(55, Dorothy::roundUptoNearest(53, 5));
-        $this->assertEquals(55, Dorothy::roundUptoNearest(54, 5));
-        $this->assertEquals(55, Dorothy::roundUptoNearest(55, 5));
-        $this->assertEquals(60, Dorothy::roundUptoNearest(56, 5));
-        $this->assertEquals(60, Dorothy::roundUptoNearest(57, 5));
-        $this->assertEquals(60, Dorothy::roundUptoNearest(58, 5));
-        $this->assertEquals(60, Dorothy::roundUptoNearest(59, 5));
-        $this->assertEquals(60, Dorothy::roundUptoNearest(60, 5));
+        $this->assertEquals(50, Dorothy::roundUpToNearest(46, 5));
+        $this->assertEquals(50, Dorothy::roundUpToNearest(47, 5));
+        $this->assertEquals(50, Dorothy::roundUpToNearest(48, 5));
+        $this->assertEquals(50, Dorothy::roundUpToNearest(49, 5));
+        $this->assertEquals(50, Dorothy::roundUpToNearest(50, 5));
+        $this->assertEquals(55, Dorothy::roundUpToNearest(51, 5));
+        $this->assertEquals(55, Dorothy::roundUpToNearest(52, 5));
+        $this->assertEquals(55, Dorothy::roundUpToNearest(53, 5));
+        $this->assertEquals(55, Dorothy::roundUpToNearest(54, 5));
+        $this->assertEquals(55, Dorothy::roundUpToNearest(55, 5));
+        $this->assertEquals(60, Dorothy::roundUpToNearest(56, 5));
+        $this->assertEquals(60, Dorothy::roundUpToNearest(57, 5));
+        $this->assertEquals(60, Dorothy::roundUpToNearest(58, 5));
+        $this->assertEquals(60, Dorothy::roundUpToNearest(59, 5));
+        $this->assertEquals(60, Dorothy::roundUpToNearest(60, 5));
     }
 }
